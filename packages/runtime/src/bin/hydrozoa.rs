b7272fc1c40631d5dd7372247aae7b1525ca329c
doinkythederp <doinkythederp@icloud.com>
2025-03-21 15:22:37 -0700

    Add backtraces to wamr errors

--- packages/runtime/src/bin/hydrozoa.rs
+++ packages/runtime/src/bin/hydrozoa.rs
@@ -18,39 +18,32 @@ extern crate alloc;
 const STACK_SIZE: u32 = 1024 * 64;
 
 fn main(_peripherals: Peripherals) {
-    if let Err(mut err) = run() {
-        // if let Some(info) = store.take_error_info() {
-        //     err = err.context(info);
-        // }
-        println!("\nError: {:?}", err);
-    }
+    run();
 }
 
-fn run() -> anyhow::Result<()> {
+fn run() {
     let wasm_bytes = platform::read_user_program();
     
     let mut runtime = Runtime::builder();
     runtime = sdk::link(runtime);
-    let runtime = runtime.build()?;
+    let runtime = runtime.build().unwrap();
 
     let module = Module::from_mut_slice(&runtime, wasm_bytes, "hydrozoa_module.wasm")
-        .context("Unable to load module")?;
+        .expect("Unable to load module");
 
-    let mut instance = Instance::new(&runtime, &module, STACK_SIZE).context("Unable to instantiate module")?;
+    let mut instance = Instance::new(&runtime, &module, STACK_SIZE).expect("Unable to instantiate module");
 
     // teavm::link_teavm(&mut *store, &mut instance).context("Unable to link teavm")?;
     // sdk::link(&mut *store, &mut instance).context("Unable to link sdk")?;
     // 
     // teavm::teamvm_main(&mut *store, &mut instance, &[]).context("Unable to run main")?;
 
-    let function = Function::find_export_func(&instance, "add")?;
+    let function = Function::find_export_func(&instance, "add").unwrap();
 
     let params: Vec<WasmValue> = vec![WasmValue::I32(3), WasmValue::I32(6)];
-    let result = function.call(&mut instance, &params)?;
+    let result = function.call(&mut instance, &params).unwrap();
     println!("add(3, 6) = {result:?}");
     // assert_eq!(result[0], WasmValue::I32(9));
-
-    Ok(())
 }
 
 #[link_section = ".code_signature"]
